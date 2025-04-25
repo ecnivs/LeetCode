ac54d9e101e87e0c49f1ba82359a68c7ae34fc28
Vince Swu <vinceswu88@gmail.com>
2025-04-26 00:59:38 +0530

    Solved using XOR

---
+++ Easy/389. Find the Difference.py
@@ -0,0 +1,7 @@
+# 389. Find the Difference
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        res = 0
+        for c in s + t:
+            res ^= ord(c)
+        return chr(res)
